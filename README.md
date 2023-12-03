6c75483897b822bdeb5b2566c0151493536847f2
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2023-12-03 13:17:39 +0200

    day3 part 1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ My Advent of Code solutions for the [year 2023](https://adventofcode.com/2023) w
 
 - [day-01/main.js](./day-01/main.js) 🌟🌟 (Day 1: Trebuchet?!)
 - [day-02/main.js](./day-02/main.js) 🌟🌟 (Day 2: Cube Conundrum)
-- [day-03/main.js](./day-03/main.js) 🚧🚧 (xxx)
+- [day-03/main.js](./day-03/main.js) 🌟 (Day 3: Gear Ratios)
 - [day-04/main.js](./day-04/main.js) 🚧🚧 (xxx)
 - [day-05/main.js](./day-05/main.js) 🚧🚧 (xxx)
 - [day-06/main.js](./day-06/main.js) 🚧🚧 (xxx)
--- day-03/main.js
+++ day-03/main.js
@@ -1,13 +1,127 @@
-const util = require('util');
+const util = require("util");
 const fs = require("fs");
 
 const path = process.argv[1].replace("/main.js", "");
 const buffer = fs.readFileSync(`${path}/input.txt`);
 const rows = buffer.toString().trimEnd().split("\n");
 
-rows.forEach(row => {
-  console.log(row);
+let sumOfPartNumbers = 0;
+const possibleSets = [];
+rows.forEach((row, rowIdx) => {
+  const numbers = row.match(/\d+/g);
+  let subRow = row;
+  numbers?.forEach((number) => {
+    const start = subRow.indexOf(`${number}`);
+    const end = start + `${number}`.length - 1;
+    const prevRow = rows[rowIdx - 1] || undefined;
+    const currRow = rows[rowIdx];
+    const nextRow = rows[rowIdx + 1] || undefined;
+    let possiblesChars = [];
+
+    if (start > 0) {
+      possiblesChars.push({
+        char: currRow.charAt(start - 1),
+        row: rowIdx,
+        col: start - 1,
+        str: "left of number",
+      });
+    }
+    if (end < subRow.length - 1) {
+      possiblesChars.push({
+        char: currRow.charAt(end + 1),
+        row: rowIdx,
+        col: end + 1,
+        str: "right of number",
+      });
+    }
+    if (!!prevRow) {
+      if (start > 0) {
+        possiblesChars.push({
+          char: prevRow.charAt(start - 1),
+          row: rowIdx - 1,
+          col: start - 1,
+          str: "top left of number",
+        });
+      }
+      Array.from({ length: end - start + 1 }, (v, i) => i + start).map((x) => {
+        possiblesChars.push({
+          char: prevRow.charAt(x),
+          row: rowIdx - 1,
+          col: x,
+          str: "top of number",
+        });
+      });
+      if (end < prevRow.length - 1) {
+        possiblesChars.push({
+          char: prevRow.charAt(end + 1),
+          row: rowIdx - 1,
+          col: end + 1,
+          str: "top right of number",
+        });
+      }
+    }
+    if (!!nextRow) {
+      if (start > 0) {
+        possiblesChars.push({
+          char: nextRow.charAt(start - 1),
+          row: rowIdx + 1,
+          col: start - 1,
+          str: "bottom left of number",
+        });
+      }
+      Array.from({ length: end - start + 1 }, (v, i) => i + start).map((x) => {
+        possiblesChars.push({
+          char: nextRow.charAt(x),
+          row: rowIdx + 1,
+          col: x,
+          str: "bottom of number",
+        });
+      });
+      if (end < nextRow.length - 1) {
+        possiblesChars.push({
+          char: nextRow.charAt(end + 1),
+          row: rowIdx + 1,
+          col: end + 1,
+          str: "bottom right of number",
+        });
+      }
+    }
+
+    possibleSets.push({
+      number: `${number}`,
+      row: row,
+      subRow: subRow,
+      start: start,
+      end: end,
+      rowIdx: rowIdx,
+      surroundings: possiblesChars,
+    });
+
+    subRow = subRow.replace(
+      `${number}`,
+      `${number}`
+        .split("")
+        .map((l) => ".")
+        .join("")
+    );
+  });
 });
 
-console.log(`Something: ${1} (part 1)`);
-console.log(`Another something: ${2} (part 2)`);
+const filteredSets = possibleSets.filter(
+  (s) => !s.surroundings.every((c) => c.char.match(/\d|\./g))
+);
+
+console.log(
+  util.inspect(filteredSets, {
+    depth: null,
+    colors: true,
+    maxArrayLength: Infinity,
+  })
+);
+
+sumOfPartNumbers = filteredSets
+  .map((x) => Number(x.number))
+  .reduce((a, b) => a + b, 0);
+
+console.log(`Puzzle 1: ${sumOfPartNumbers}`);
+console.log(`Puzzle 2: ${2}`);
