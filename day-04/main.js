ed636cfac832994afd85e12d831771f2fe6f99a4
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2023-12-04 15:33:21 +0200

    day 4 part 2

--- day-04/main.js
+++ day-04/main.js
@@ -6,27 +6,43 @@ const buffer = fs.readFileSync(`${path}/input.txt`);
 const rows = buffer.toString().trimEnd().split("\n");
 
 let totalPoints = 0;
-rows.forEach((row) => {
-  const winningNumbers = row
+let totalCards = Array(rows.length).fill(1);
+
+rows.forEach((card) => {
+  const cardNumber = Number(card.split(":")[0].replace(/[^0-9]/g, ""));
+  const winningNumbers = card
     .split(":")[1]
     .split("|")[0]
     .split(" ")
     .filter((x) => x !== "")
     .map((x) => Number(x));
-  const cardNumbers = row
+  const cardNumbers = card
     .split(":")[1]
     .split("|")[1]
     .split(" ")
     .filter((x) => x !== "")
     .map((x) => Number(x));
+  const wonCards = cardNumbers.filter((n) => winningNumbers.includes(n));
 
-  const winnings = cardNumbers.filter((n) => winningNumbers.includes(n));
-  const points = winnings.length
-    ? winnings.reduce((a, b, i) => (i < 1 ? a : a * 2), 1)
+  // part 1
+  const points = wonCards.length
+    ? wonCards.reduce((a, b, i) => (i < 1 ? a : a * 2), 1)
     : 0;
-
   totalPoints += points;
+
+  // part 2
+  for (let i = 1; i < wonCards.length + 1; i++) {
+    totalCards[cardNumber - 1 + i] += totalCards[cardNumber - 1];
+  }
 });
 
+console.log(
+  util.inspect(totalCards, {
+    depth: null,
+    colors: true,
+    maxArrayLength: Infinity,
+  })
+);
+
 console.log(`Puzzle 1: ${totalPoints}`);
-console.log(`Puzzle 2: ${2}`);
+console.log(`Puzzle 2: ${totalCards.reduce((a, b) => a + b, 0)}`);
