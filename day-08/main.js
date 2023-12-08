a7c9dde5ad5ed989dc068cd3417d068c4268fe93
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2023-12-08 15:33:04 +0200

    day 8 part 1 parsing data

--- day-08/main.js
+++ day-08/main.js
@@ -5,9 +5,16 @@ const path = process.argv[1].replace("/main.js", "");
 const buffer = fs.readFileSync(`${path}/input.txt`);
 const rows = buffer.toString().trimEnd().split("\n");
 
-rows.forEach((row) => {
-  console.log(row);
+const lrs = rows[0].split("");
+const nodes = rows.slice(2).map((row) => {
+  const node = row.split(" = ")[0].trim();
+  const left = row.split(" = ")[1].split(", ")[0].replace("(", "").trim();
+  const right = row.split(" = ")[1].split(", ")[1].replace(")", "").trim();
+  return [node, left, right];
 });
+
+console.log(lrs);
+console.log(nodes);
 
 console.log(`Puzzle 1: ${1}`);
 console.log(`Puzzle 2: ${2}`);
