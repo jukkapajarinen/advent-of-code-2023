4addf6f649fc03e5d89cb8f1433e201a5cdd4340
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2023-12-08 14:52:22 +0200

    day 5 part 2. decent performance

--- day-05/main.js
+++ day-05/main.js
@@ -5,47 +5,75 @@ const path = process.argv[1].replace("/main.js", "");
 const buffer = fs.readFileSync(`${path}/input.txt`);
 const rows = buffer.toString().trimEnd().split("\n");
 
-const parseMapRow = (mapRow) =>
-  mapRow
-    .split(" ")
-    .map((x) => x.trim())
-    .filter((x) => x !== "")
-    .map((x) => Number(x));
+const getLowestLocationNumber = (seeds, maps) => {
+  let lowestLocationNumber = Infinity;
+  for (const seed of seeds) {
+    for (let i = seed.src; i <= seed.src + (seed.range - 1); i++) {
+      let currentValue = i;
+      for (const map of maps) {
+        for (const ranges of map) {
+          if (currentValue >= ranges.src && currentValue <= ranges.srcMax) {
+            const srcIdx = currentValue - ranges.src;
+            currentValue = ranges.dest + srcIdx;
+            break;
+          }
+        }
+      }
 
-const seeds = rows[0]
-  .split(":")[1]
-  .trim()
-  .split(" ")
-  .map((seed) => Number(seed));
-const maps = rows.slice(1).filter((row) => row !== "");
-
-let lowestLocationNumber = Infinity;
-seeds.forEach((seed) => {
-  let mapIdx = -1;
-  let mapMatchFound = false;
-  let currentValue = seed;
-  maps.forEach((mapRow) => {
-    if (mapRow.includes(" map:")) {
-      mapIdx++;
-      mapMatchFound = false;
-      return;
+      if (currentValue < lowestLocationNumber) {
+        lowestLocationNumber = currentValue;
+      }
     }
+  }
+  return lowestLocationNumber;
+};
 
-    const [dest, src, range] = parseMapRow(mapRow);
-    const destMax = dest + (range - 1);
-    const srcMax = src + (range - 1);
-
-    if (!mapMatchFound && currentValue >= src && currentValue <= srcMax) {
-      const srcIdx = currentValue - src;
-      currentValue = dest + srcIdx;
-      mapMatchFound = true;
+// maps are same for part 1 and 2
+const maps = [];
+let currentMap = [];
+rows
+  .slice(2)
+  .filter((row) => !row.includes(" map:"))
+  .concat([""])
+  .forEach((mapRow, idx) => {
+    if (mapRow === "") {
+      maps.push(currentMap);
+      currentMap = [];
+    } else {
+      const [dest, src, range] = mapRow
+        .split(" ")
+        .map((x) => x.trim())
+        .filter((x) => x !== "")
+        .map((x) => Number(x));
+      const destMax = dest + (range - 1);
+      const srcMax = src + (range - 1);
+      currentMap.push({ dest, destMax, src, srcMax, range });
     }
   });
 
-  if (currentValue < lowestLocationNumber) {
-    lowestLocationNumber = currentValue;
-  }
-});
+// Part 1
+const seeds1 = rows[0]
+  .split(":")[1]
+  .trim()
+  .split(" ")
+  .map((seed) => {
+    return { src: Number(seed), srcMax: Number(seed), range: 1 };
+  });
+const location1 = getLowestLocationNumber(seeds1, maps);
+console.log(`Puzzle 1: ${location1}`);
 
-console.log(`Puzzle 1: ${lowestLocationNumber}`);
-console.log(`Puzzle 2: ${2}`);
+// part 2
+const seeds2 = [];
+const seedsAndRanges = rows[0]
+  .split(":")[1]
+  .trim()
+  .split(" ")
+  .map((x) => Number(x));
+for (let i = 0; i < seedsAndRanges.length; i += 2) {
+  const src = seedsAndRanges[i];
+  const range = seedsAndRanges[i + 1];
+  const srcMax = src + range - 1;
+  seeds2.push({ src: seedsAndRanges[i], srcMax: srcMax, range: range });
+}
+const location2 = getLowestLocationNumber(seeds2, maps);
+console.log(`Puzzle 2: ${location2}`);
